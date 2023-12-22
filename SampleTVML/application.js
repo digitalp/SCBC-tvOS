afbbb2fedf737b2d787490ef270ac3fd654ecf2c
digitalp <penntangu@gmail.com>
2023-12-22 14:45:45 +0000

    Update application.js

--- SampleTVML/application.js
+++ SampleTVML/application.js
@@ -9,13 +9,17 @@ App.onLaunch = function(options) {
     ];
 
     evaluateScripts(javascriptFiles, function(success) {
-        if(success) {
+        if (success) {
             resourceLoader = new ResourceLoader(options.BASEURL);
+
+            // Load EPG data and then load the main template
             loadEPGData(`${options.BASEURL}scbcguide.xml`, function(epgData) {
-                var mainPageTemplate = createMainPageTemplate(epgData);
-                var doc = Presenter.makeDocument(mainPageTemplate);
-                doc.addEventListener("select", Presenter.load.bind(Presenter));
-                Presenter.pushDocument(doc);
+                var processedEPGData = processEPGData(epgData);
+                resourceLoader.loadResource(`${options.BASEURL}templates/MainPageTemplate.xml.js`, function(resource) {
+                    var doc = Presenter.makeDocument(resource, processedEPGData);
+                    doc.addEventListener("select", Presenter.load.bind(Presenter));
+                    Presenter.pushDocument(doc);
+                });
             });
         } else {
             var errorDoc = createAlert("Evaluate Scripts Error", "Error attempting to evaluate external JavaScript files.");
@@ -31,7 +35,7 @@ function loadEPGData(url, callback) {
             if (xhr.status === 200) {
                 var parser = new DOMParser();
                 var xmlDoc = parser.parseFromString(xhr.responseText, "application/xml");
-                callback(xmlDoc); // Process and extract EPG data
+                callback(xmlDoc); // Pass the XML document for further processing
             } else {
                 console.error("Failed to fetch EPG data");
             }
@@ -41,50 +45,23 @@ function loadEPGData(url, callback) {
     xhr.send();
 }
 
-function createMainPageTemplate(epgXml) {
-    var programmes = epgXml.getElementsByTagName("programme");
-    var now = new Date();
-    var upcomingProgrammes = [];
-
-    for (var i = 0; i < programmes.length; i++) {
-        var programme = programmes[i];
-        var startDate = parseDate(programme.getAttribute("start"));
-        var endDate = parseDate(programme.getAttribute("stop"));
-
-        if (now >= startDate && now <= endDate || now < startDate) {
-            upcomingProgrammes.push(programme);
-            if (upcomingProgrammes.length === 4) break; // Current + next 3 shows
-        }
-    }
-
-    return generateEPGTVML(upcomingProgrammes);
-}
-
-function parseDate(dateString) {
-    // Parse the date string from the XMLTV format to a JavaScript Date object
-    // Implement this based on the format of the dates in your XMLTV data
-    // Example: "20231222060000 +0100" --> JavaScript Date object
-    // Return the parsed Date object
-}
-
-function generateEPGTVML(programmes) {
-    var tvmlString = `<?xml version="1.0" encoding="UTF-8" ?><document><stackTemplate><banner><title>Program Guide</title></banner><collectionList><grid><section>`;
-
-    for (var i = 0; i < programmes.length; i++) {
-        var programme = programmes[i];
-        var title = programme.getElementsByTagName("title")[0].textContent;
-        var desc = programme.getElementsByTagName("desc")[0].textContent;
-
-        tvmlString += `<lockup><img src="your_image_url_here" width="500" height="308" /><title>${title}</title><description>${desc}</description></lockup>`;
-    }
-
-    tvmlString += `</section></grid></collectionList></stackTemplate></document>`;
-
-    return tvmlString;
+function processEPGData(epgXml) {
+    // Process the XML document and extract EPG data
+    // This function should return processed data in a format suitable for MainPageTemplate
+    // Implement the logic based on your EPG data structure and requirements
 }
 
 var createAlert = function(title, description) {
-    var alertString = `<?xml version="1.0" encoding="UTF-8" ?><document><alertTemplate><title>${title}</title><description>${description}</description><button><text>OK</text></button></alertTemplate></document>`;
+    var alertString = `<?xml version="1.0" encoding="UTF-8" ?>
+      <document>
+        <alertTemplate>
+          <title>${title}</title>
+          <description>${description}</description>
+          <button>
+            <text>OK</text>
+          </button>
+        </alertTemplate>
+      </document>`;
 
     var parser = new DOMParser();
     var alertDoc = parser.parseFromString(alertString, "application/xml");
