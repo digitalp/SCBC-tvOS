55716e514f891096c5bd9bed90ce95318bbba7f1
digitalp <penntangu@gmail.com>
2023-12-22 13:04:16 +0000

    Update MainPageTemplate.xml.js

--- SampleTVML/templates/MainPageTemplate.xml.js
+++ SampleTVML/templates/MainPageTemplate.xml.js
@@ -9,28 +9,142 @@
 
 
 var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
-<document>
-    <menuBarTemplate>
-        <menuBar>
-            <menuItem id="LIVE" data-identifier="list">
-                <title>Top Movies</title>
-                <lockup videoURL="https://cdn.scbcmedia.com/scbctelevision/c9a1fdac6e082dd89e7173244f34d7b3.sdp/playlist.m3u8">
-                  <img src="${this.BASEURL}images/scbcthumb.png" width="500" height="308" />
-                </lockup>
-            </menuItem>
-            <menuItem id="Ask Docta!" data-identifier="index">
-                <title>Genres</title>
-            </menuItem>
-            <menuItem id="navigation_search" data-identifier="search">
-                <title>Search</title>
-            </menuItem>
-            <menuItem id="navigation_edit" data-identifier="edit">
-                <title>Edit</title>
-            </menuItem>
-            <menuItem id="navigation_settings_add" data-identifier="add_settings">
-                <title>Add Settings</title>
-            </menuItem>
-        </menuBar>
-    </menuBarTemplate>
-</document>
+  <document>
+    <catalogTemplate>
+      <banner>
+        <title>SCBC Television</title>
+      </banner>
+      <list>
+        <section>
+      <listItemLockup>
+        <title>LIVE</title>
+        <decorationLabel>13</decorationLabel>
+            //1. add from here
+        <relatedContent>
+          <grid>
+            <section>
+                  //2
+          <lockup videoURL="https://cdn.scbcmedia.com/scbctelevision/c9a1fdac6e082dd89e7173244f34d7b3.sdp/playlist.m3u8">
+            <img src="${this.BASEURL}images/scbcthumb.png" width="500" height="308" />
+          </lockup>
+          <lockup videoURL="https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8">
+            <img src="${this.BASEURL}images/ryan.png" width="500" height="308" />
+          </lockup>
+              <lockup videoURL="https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8">
+            <img src="${this.BASEURL}images/matthijs.png" width="500" height="308" />
+          </lockup>
+          <lockup videoURL="https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8">
+            <img src="${this.BASEURL}images/vicki.png" width="500" height="308" />
+          </lockup>
+              <lockup videoURL="https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8">
+            <img src="${this.BASEURL}images/alexis.png" width="500" height="308" />
+              </lockup>
+          <lockup videoURL="https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8">
+            <img src="${this.BASEURL}images/marin.png" width="500" height="308" />
+          </lockup>
+          <lockup videoURL="https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8">
+            <img src="${this.BASEURL}images/chris.png" width="500" height="308" />
+              </lockup>
+          <lockup videoURL="https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8">
+            <img src="${this.BASEURL}images/cesare.png" width="500" height="308" />
+          </lockup>
+          <lockup videoURL="https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8">
+            <img src="${this.BASEURL}images/ellen.png" width="500" height="308" />
+          </lockup>
+          <lockup videoURL="https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8">
+            <img src="${this.BASEURL}images/jake.png" width="500" height="308" />
+          </lockup>
+          <lockup videoURL="https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8">
+            <img src="${this.BASEURL}images/kim.png" width="500" height="308" />
+          </lockup>
+              <lockup videoURL="https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8">
+            <img src="${this.BASEURL}images/tammy.png" width="500" height="308" />
+          </lockup>
+          <lockup videoURL="https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8">
+            <img src="${this.BASEURL}images/saul.png" width="500" height="308" />
+          </lockup>
+        </section>
+          </grid>
+        </relatedContent>
+      </listItemLockup>
+  
+        <listItemLockup>
+          <title>Funny Videos</title>
+          <decorationLabel>3</decorationLabel>
+              //1. add from here
+          <relatedContent>
+            <grid>
+              <section>
+                    //2
+                      <lockup videoURL="https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8">
+                        <img src="${this.BASEURL}images/kim.png" width="500" height="308" />
+                      </lockup>
+                          <lockup videoURL="https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8">
+                        <img src="${this.BASEURL}images/tammy.png" width="500" height="308" />
+                      </lockup>
+                      <lockup videoURL="https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8">
+                        <img src="${this.BASEURL}images/saul.png" width="500" height="308" />
+                      </lockup>
+  
+          </section>
+            </grid>
+          </relatedContent>
+        </listItemLockup>
+  
+          <listItemLockup>
+            <title>Educational Videos</title>
+            <decorationLabel>4</decorationLabel>
+                //1. add from here
+            <relatedContent>
+              <grid>
+                <section>
+                      //2
+                      <lockup videoURL="https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8">
+                        <img src="${this.BASEURL}images/cesare.png" width="500" height="308" />
+                      </lockup>
+                      <lockup videoURL="https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8">
+                        <img src="${this.BASEURL}images/ellen.png" width="500" height="308" />
+                      </lockup>
+                      <lockup videoURL="https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8">
+                        <img src="${this.BASEURL}images/jake.png" width="500" height="308" />
+                      </lockup>
+                      <lockup videoURL="https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8">
+                        <img src="${this.BASEURL}images/kim.png" width="500" height="308" />
+                      </lockup>
+    
+            </section>
+              </grid>
+            </relatedContent>
+          </listItemLockup>
+  
+            <listItemLockup>
+              <title>Anime Videos</title>
+              <decorationLabel>3</decorationLabel>
+                  //1. add from here
+              <relatedContent>
+                <grid>
+                  <section>
+                        //2
+                                  <lockup videoURL="https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8">
+                                    <img src="${this.BASEURL}images/vicki.png" width="500" height="308" />
+                                  </lockup>
+                                      <lockup videoURL="https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8">
+                                    <img src="${this.BASEURL}images/alexis.png" width="500" height="308" />
+                                      </lockup>
+                                  <lockup videoURL="https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8">
+                                    <img src="${this.BASEURL}images/marin.png" width="500" height="308" />
+                                  </lockup>
+      
+              </section>
+                </grid>
+              </relatedContent>
+            </listItemLockup>
+  
+        </section>
+      </list>
+    </catalogTemplate>
+  </document>`
 }
+
+
+
