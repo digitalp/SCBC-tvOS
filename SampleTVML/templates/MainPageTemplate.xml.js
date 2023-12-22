b6fb18b1315298a6e3dcab07394aaa94889e19be
digitalp <penntangu@gmail.com>
2023-12-22 15:31:44 +0000

    Update MainPageTemplate.xml.js

--- SampleTVML/templates/MainPageTemplate.xml.js
+++ SampleTVML/templates/MainPageTemplate.xml.js
@@ -11,7 +11,7 @@
 var Template = function() { 
     return `<?xml version="1.0" encoding="UTF-8" ?>
     <document>
-      <catalogTemplate
+      <catalogTemplate>
           <background>
               <img src="${this.BASEURL}images/SCBC-Background-FullHD13.png"/>
           </background>
